88ddafc47a3645acc28339b1d5c5c506452e11ef
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2020-07-22 22:38:54 +0500

    update Learn about twoway databinding or local refrence

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,32 +1,50 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewEncapsulation, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
   templateUrl: './cockpit.component.html',
-  styleUrls: ['./cockpit.component.css']
+  styleUrls: ['./cockpit.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class CockpitComponent implements OnInit {
   // here we are passing outside the component
   @Output() serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
   @Output() blueprintCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
-  newServerName = '';
-  newServerContent = '';
+  // newServerName = '';
+  // newServerContent = '';
+
+  @ViewChild('inputContentRef') inputContentRef: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
   }
-  onAddServer() {
+
+  onAddServer(nameInput: HTMLInputElement) {
     this.serverCreated.emit({
-      serverName: this.newServerName,
-      serverContent: this.newServerContent
+      serverName: nameInput.value,
+      serverContent: this.inputContentRef.nativeElement.value
     });
   }
-  onAddBluePrint() {
+  onAddBluePrint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
-      serverName: this.newServerName,
-      serverContent: this.newServerContent
+      serverName: nameInput.value,
+      serverContent: this.inputContentRef.nativeElement.value
     });
   }
+
+  // Done All with the help of Two Way DataBinding 
+  // onAddServer() {
+  //   this.serverCreated.emit({
+  //     serverName: this.newServerName,
+  //     serverContent: this.newServerContent
+  //   });
+  // }
+  // onAddBluePrint() {
+  //   this.blueprintCreated.emit({
+  //     serverName: this.newServerName,
+  //     serverContent: this.newServerContent
+  //   });
+  // }
 
 }
